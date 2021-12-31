84c8a659d9a4e4b75527cc67a02cd3a6a35a27a7
Mayank Gajbhiye <mayank1398@gmail.com>
2021-12-31 23:00:33 +0530

    Disable seliux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/gucci
 TARGET_KERNEL_CONFIG := gucci_defconfig
 BOARD_KERNEL_CMDLINE += phy-msm-usb.floated_charger_enable=1
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 
